69e52eb62fc377ca8a4e32c898381bdf608c1577
thuylinh5672004 <164979311+thuylinh5672004@users.noreply.github.com>
2024-11-04 11:32:26 +0700

    [200~Daothithuylinhproject2_2210900036~

---
+++ Daothithuylinhproject2_2210900036/Daothithuylinhproject2_2210900036/Areas/DTLinhAdmin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Daothithuylinhproject2_2210900036.Areas.DTLinhAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: DTLinhAdmin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
